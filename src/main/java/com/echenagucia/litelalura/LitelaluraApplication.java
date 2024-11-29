bb425761646bbdde1e50aa86991e4dec0804bfba
Eduardo Echenagucia <eduardonyx.ee@gmail.com>
2024-11-29 15:10:32 -0300

    Version Inicial

---
+++ src/main/java/com/echenagucia/litelalura/LitelaluraApplication.java
@@ -0,0 +1,13 @@
+package com.echenagucia.litelalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitelaluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitelaluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echenagucia/litelalura/LitelaluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echenagucia.litelalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitelaluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
